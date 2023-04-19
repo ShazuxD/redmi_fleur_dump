4e13ae16456b2c3564e54f91b9f227d7d090edda
ShazuxD <116494974+ShazuxD@users.noreply.github.com>
2023-04-20 00:14:38 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export TWRP_MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb"
-export TWRP_BRANCH="android-11.0"
-export DT_LINK="https://github.com/ShazuxD/device_realme_RMX1801-twrp-12.1"
-export DT_BRANCH="11_12.1"
-export DEVICE="RMX1801"
-export OEM="realme"
-export TARGET="recoveryimage"
-export MAKEFILE="omni_RMX1801"
-export OUTPUT="P*.zip"
+export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
+export TWRP_BRANCH="twrp-12.1"
+export DT_LINK="https://github.com/ShazuxD/recovery-pbrb"
+export DT_BRANCH="12"
+export DEVICE="fleur"
+export OEM="xiaomi"
+export TARGET="bootimage"
+export MAKEFILE="twrp_fleur"
+export OUTPUT="boot.img"
 #export KERNEL="" if exist
 #export KERNEL_PATH="" if exist
 
