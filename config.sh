62b160fc59ccc0d2e91cede210d26a40c881c1be
ShazuxD <ShazuxD7@gmail.com>
2023-04-19 16:28:27 +0530

    build pbrp 11

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
-export TWRP_BRANCH="twrp-11"
+export TWRP_MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb"
+export TWRP_BRANCH="android-11.0"
 export DT_LINK="https://github.com/ShazuxD/device_realme_RMX1801-twrp-12.1"
-export DT_BRANCH="11"
+export DT_BRANCH="11_12.1"
 export DEVICE="RMX1801"
 export OEM="realme"
 export TARGET="recoveryimage"
-export MAKEFILE="twrp_RMX1801"
-export OUTPUT="rec*.img"
+export MAKEFILE="omni_RMX1801"
+export OUTPUT="P*.zip"
 #export KERNEL="" if exist
 #export KERNEL_PATH="" if exist
 
