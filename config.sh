3d4912a82b6e3cf5f099c28d5a3701efbefb1938
ShazuxD <ShazuxD7@gmail.com>
2023-04-20 15:47:42 +0530

    build with ccache

--- config.sh
+++ config.sh
@@ -2,12 +2,13 @@
 export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
 export TWRP_BRANCH="twrp-12.1"
 export DT_LINK="https://github.com/ShazuxD/recovery-pbrb"
-export DT_BRANCH="12"
+export DT_BRANCH="test"
 export DEVICE="fleur"
 export OEM="xiaomi"
 export TARGET="bootimage"
 export MAKEFILE="twrp_fleur"
 export OUTPUT="boot.img"
+export URL="https://mirror-leech-kingdom.site/0:/9%20.Other//ccache.tar.gz"
 #export KERNEL="" if exist
 #export KERNEL_PATH="" if exist
 
--- script/checks.sh
+++ script/checks.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Do not allow curl
+source $CONFIG
 curl_check=$(grep 'curl ' $CONFIG | wc -l)
 if [[ $curl_check -gt 0 ]]; then
     echo -e "Please dont use \'curl\' in $CONFIG".
     exit 1
 fi
+
+cd /tmp
+time aria2c $URL -x16 -s50
+time tar xf ccache.tar.gz
+echo
