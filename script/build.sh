1a41b7d2f6f77b375e870c41254492b916f9f9d6
ShazuxD <116494974+ShazuxD@users.noreply.github.com>
2023-04-26 23:59:41 +0530

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -5,7 +5,8 @@ source $CONFIG
 
 # Change to the Source Directry
 cd /tmp/ci
-
+git fetch https://gerrit.twrp.me/android_system_vold refs/changes/40/5540/6 && git cherry-pick FETCH_HEAD
+git fetch https://gerrit.twrp.me/android_bootable_recovery refs/changes/05/5405/23 && git cherry-pick FETCH_HEAD
 # Prepare the Build Environment
 source build/envsetup.sh
 lunch ${MAKEFILE}-eng
