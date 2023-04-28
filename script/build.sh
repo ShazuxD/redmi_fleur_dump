c2cec639dc7bfe820dc365084a5b2072d07be60c
ShazuxD <116494974+ShazuxD@users.noreply.github.com>
2023-04-28 17:57:44 +0530

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -4,6 +4,13 @@
 source $CONFIG
 
 # Change to the Source Directry
+
+cd /tmp/ci/external/noto-fonts/cjk
+wget https://github.com/ShazuxD/extras/raw/main/NotoSansEthiopic-Regular-VF.ttf
+wget https://github.com/ShazuxD/extras/raw/main/NotoSansEthiopic-Regular.ttf
+cd /tmp/ci/external/noto-fonts/other
+wget https://github.com/ShazuxD/extras/raw/main/NotoSansEthiopic-Regular-VF.ttf
+wget https://github.com/ShazuxD/extras/raw/main/NotoSansEthiopic-Regular.ttf
 cd /tmp/ci
 # Prepare the Build Environment
 source build/envsetup.sh
